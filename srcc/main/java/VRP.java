e036f533d323e7bd84844a624e831799ac4f53b8
DimiChatzipavlis <t8210163@aueb.gr>
2025-01-14 19:56:13 +0200

    Fixing refill issue in VRP's solver

--- srcc/main/java/VRP.java
+++ srcc/main/java/VRP.java
@@ -1,5 +1,5 @@
-import java.util.List;
 import java.util.ArrayList;
+import java.util.List;
 
 public class VRP {
     private List<Node> nodes;
@@ -70,67 +70,80 @@ public class VRP {
     }
 
     // Nearest Neighbor Algorithm to construct a simple route for each vehicle
-    private List<Node> nearestNeighbor(int vehicleIndex) {
+   // Nearest Neighbor Algorithm to construct a route for each vehicle
+private List<Node> nearestNeighbor(int vehicleIndex) {
+    Vehicle vehicle = vehicles.get(vehicleIndex);
+    Node depot = nodes.get(0);
+    List<Node> route = new ArrayList<>();
+    boolean[] visited = new boolean[nodes.size()];
 
-        // Create the Vehicle object for the given index
-        Vehicle vehicle = vehicles.get(vehicleIndex);
+    // Start from the depot
+    Node currentNode = depot;
+    route.add(currentNode);
+    visited[currentNode.getIndex()] = true;
+    double remainingCapacity = vehicle.getCapacity();
 
-        // Identify the depot (always node 0)
-        Node depot = nodes.get(0);
+    // Continue constructing the route until all demands are met or no more can be served
+    while (true) {
+        double minDistance = Double.MAX_VALUE;
+        Node nearestNode = null;
 
-        List<Node> route = new ArrayList<>();
-        boolean[] visited = new boolean[nodes.size()];
-
-        // Starting from node 0 (the depot)
-        Node currentNode = depot;
-        route.add(currentNode);
-        visited[currentNode.getIndex()] = true;
-        double remainingCapacity = vehicle.getCapacity();
-
-        // Apply nearest neighbor heuristic for vehicle route
-        while (route.size() < nodes.size()) {
-            double minDistance = Double.MAX_VALUE;
-            Node nearestNode = null;
-
-            // Search for the nearest unvisited node
-            for (Node node : nodes) {
-                if (!visited[node.getIndex()] && node != depot && remainingDemand[node.getIndex()] > 0) {
-                    double distance = distanceMatrix[currentNode.getIndex()][node.getIndex()];
-                    if (distance < minDistance && remainingDemand[node.getIndex()] <= remainingCapacity) {
-                        minDistance = distance;
-                        nearestNode = node;
-                    }
+        // Find the nearest unvisited node with unserved demand
+        for (Node node : nodes) {
+            if (!visited[node.getIndex()] && node != depot && remainingDemand[node.getIndex()] > 0) {
+                double distance = distanceMatrix[currentNode.getIndex()][node.getIndex()];
+                // Check if the vehicle can serve the demand of this node
+                if (distance < minDistance && remainingDemand[node.getIndex()] <= remainingCapacity) {
+                    minDistance = distance;
+                    nearestNode = node;
                 }
             }
-
-            // If no more nodes can be served, return to depot
-            if (nearestNode == null) {
-                if (currentNode != depot) {
-                    route.add(depot); // Return to depot to refill
-                    remainingCapacity = vehicle.getCapacity(); // Refill the vehicle
-                    currentNode = depot;
-                } else {
-                    break; // No more nodes can be served, exit loop
-                }
-            } else {
-                // Serve the nearest node
-                route.add(nearestNode);
-                visited[nearestNode.getIndex()] = true;
-                remainingCapacity -= nearestNode.getDemand();
-                remainingDemand[nearestNode.getIndex()] = 0; // Mark demand as fully served
-                currentNode = nearestNode;
-
-                // If capacity is exhausted, return to the depot
-                if (remainingCapacity <= 0) {
-                    route.add(depot);
-                    remainingCapacity = vehicle.getCapacity(); // Refill capacity
-                    currentNode = depot;
-                }
-            }
-
         }
-        return route;
+
+        if (nearestNode == null) {
+            // No more reachable nodes: return to depot if necessary or break loop
+            if (currentNode != depot) {
+                route.add(depot); // Return to depot to refill
+                remainingCapacity = vehicle.getCapacity(); // Refill capacity
+                currentNode = depot;
+            } else {
+                break; // No nodes can be served; exit loop
+            }
+        } else {
+            // Serve the nearest node
+            route.add(nearestNode);
+            visited[nearestNode.getIndex()] = true;
+            remainingCapacity -= nearestNode.getDemand();
+            remainingDemand[nearestNode.getIndex()] = 0; // Mark demand as fully served
+            currentNode = nearestNode;
+
+            // If capacity is insufficient for further deliveries, return to depot
+            if (remainingCapacity <= 0) {
+                route.add(depot);
+                remainingCapacity = vehicle.getCapacity(); // Refill capacity
+                currentNode = depot;
+            }
+        }
+
+        // Check if all demands are met
+        boolean allDemandsMet = true;
+        for (double demand : remainingDemand) {
+            if (demand > 0) {
+                allDemandsMet = false;
+                break;
+            }
+        }
+        if (allDemandsMet) {
+            if (currentNode != depot) {
+                route.add(depot); // Ensure route ends at depot
+            }
+            break;
+        }
     }
+
+    return route;
+}
+
 
     // Method to set the distance matrix (populate from user input or calculated
     // values)
